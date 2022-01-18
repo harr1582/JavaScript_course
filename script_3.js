fb0ef8c8cc8f9a33b35cd5beb7591d8c6db7e1cf
harr1582 <46164650+harr1582@users.noreply.github.com>
2022-01-17 21:02:26 -0500

    Newest version

--- script_3.js
+++ script_3.js
@@ -95,18 +95,145 @@
 
 
 //shorter, better:
-const calcAve = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+// const calcAve = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
-const dolphAve = calcAve(85, 54, 41)
-const koalAve = calcAve(23, 34, 27)
+// const dolphAve = calcAve(85, 54, 41)
+// const koalAve = calcAve(23, 34, 27)
 
-const checkWinner2 = function (avgDolph, avgKo) {
-    if (avgDolph >= (2 * avgKo)) {
-        console.log(`Dolphs win`)
-    }
-    else if (avgKo >= (2 * avgDolph)) {
-        console.log('Kos win')
-    }
-    else { console.log('losers') }
+// const checkWinner2 = function (avgDolph, avgKo) {
+//     if (avgDolph >= (2 * avgKo)) {
+//         console.log(`Dolphs win`)
+//     }
+//     else if (avgKo >= (2 * avgDolph)) {
+//         console.log('Kos win')
+//     }
+//     else { console.log('losers') }
+// }
+// checkWinner2(dolphAve, koalAve)
+
+//Arrays
+
+// const populations = [333000000, 1000000000, 59550000, 47350000]
+// //if (populations.length = 4) { console.log('True') } else { console.log('False') }
+
+// function percentageOfWorld1(population) {
+//     return ((population / 7753000000) * 100)
+// }
+// const percentages = [percentageOfWorld1(populations[0]), percentageOfWorld1(populations[1]), percentageOfWorld1(populations[2]), percentageOfWorld1(populations[3])]
+
+// console.log(populations)
+// console.log(percentages)
+
+//Array Operations
+
+// const neighbors = ['Mexico', 'Canada']
+
+// //Add element to array
+// neighbors.push('Utopia')
+// console.log(neighbors)
+
+// //Remove Utopia from array
+// neighbors.pop()
+// console.log(neighbors)
+
+// if (neighbors.includes('Germany')) { console.log('Probably Central European') } else { console.log('Probably not a central European Country') }
+
+// console.log(neighbors.indexOf('Canada'))
+
+// neighbors[1] = 'NORTHcAnada'
+// console.log(neighbors)
+
+//Coding Challenge 2
+
+// const check = 430;
+// const tip = .15 * check;
+// const tip2 = .20 * check;
+
+// if (50 <= check <= 300) {
+//     console.log(`The bill was ${check}, the tip was ${tip}, and the total value was ${check + tip}`);
+// } else {
+//     console.log(`The bill was ${check}, the tip was ${tip2}, and the total value was ${check + tip2}`);
+// }
+
+// function calcTip(check) {
+
+//     if (50 <= check <= 300) {
+//         const tip = (check * .15);
+//         return (tip + check);
+//     } else {
+//         const tip = (check * .20);
+//         return (tip + check);
+//     }
+// }
+// console.log(calcTip(100))
+
+// const bills = [125, 555, 44]
+// const totals = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+// const tips = [calcTip(bills[0]) - (bills[0]), calcTip(bills[1]) - (bills[1]), calcTip(bills[2]) - (bills[2])]
+// console.log(bills)
+// console.log(totals)
+// console.log(tips)
+
+// //I forgot about the ? to return true/false:
+
+// const calcTip = function (bill) {
+//     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// }
+
+// Objects - define key value pairs. Use curly brackets
+
+//Object literals
+// const jonas = {
+//     firstName: 'Jonas',
+//     lastName: 'Schmedtmann',
+//     birthYear: 1989,
+//     job: 'teacher',
+//     friends: ['Steve', 'Lexy', 'Sarah'],
+//     hasDriverslicense: true,
+//     calcAge: function () {
+//         //console.log(this);
+//         return 2022 - this.birthYear;
+//     },
+
+//     getSummary: function () {
+//         return `${this.firstName} is a ${this.calcAge()} year old who has ${this.hasDriverslicense ? `a` : 'no'} driver's license.` //need to have the parentheses after functions
+//     }
+
+// };
+
+// console.log(jonas.getSummary())
+// console.log(`${jonas.firstName} is a ${jonas.calcAge()} year old who has ${jonas.license} driver's license`)
+
+
+//Getting items from objects
+// console.log(jonas.lastName);
+// console.log(jonas['lastName']);
+
+// const nameKey = 'Name';
+// console.log(nameKey)
+// console.log(jonas[`first` + nameKey])
+//This translates to console.log(jonas[firstName])
+
+// const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends');
+
+// console.log(`${jonas.firstName} has ${jonas['friends'].length} friends, and his best friend is ${jonas.friends[0]}`)
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+
+const myCountry = {
+    country: 'US',
+    capital: 'DC',
+    language: null,
+    population: 333000000,
+    neighbors: [`Mexico`, `Canada`],
+    describe: function () {
+        return `${this.country} has ${this.population} ${this.language} speaking people, ${this.neighbors.length} neighboring countries, and a capital called ${this.capital}`
+    },
+
 }
-checkWinner2(dolphAve, koalAve)
+
+console.log(myCountry.describe())
+
